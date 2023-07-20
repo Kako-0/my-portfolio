71e27e294b5beacd26be7cee626866b5470fc6b7
Kayro França <kayro.dev@gmail.com>
2023-07-20 16:42:10 -0300

    Feat(): Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import Blobs from "@/components/Blobs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Olá, meu nome é Kayro França.</h1>
-      <h2>Um desenvolvedor web focado em frontend.</h2>
       <Blobs />
+      <div className="grid gap-4 place-items-center">
+        <h1 className=" font-extrabold text-5xl text-clip bg-clip-text mix-blend-difference invert tracking-wide text-center">
+          Olá, meu nome é Kayro França.
+        </h1>
+        <h2 className=" font-bold text-3xl text-clip bg-clip-text text-center mix-blend-difference invert">
+          Um desenvolvedor web focado em frontend.
+        </h2>
+      </div>
     </main>
   );
 }
