73668c3b461697316888bdac3e9a5ab3cffc43d4
Kayro França <kayro.dev@gmail.com>
2023-08-02 21:08:38 -0300

    reset hompage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Blobs from "@/components/Blobs";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Blobs />
       <main className="flex flex-col items-start w-full h-[85vh] relative p-40">
         <div className="grid gap-4 place-items-start p-8 bg-blue-200 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
           <h1 className=" font-extrabold text-5xl tracking-wide">
@@ -13,7 +11,7 @@ export default function Home() {
             Eu sou <span className="text-blue-900">Kayro França</span>. <br />
             Um desenvolvedor web focado em frontend.
           </h1>
-          <h2 className=" font-bold text-3xl text-clip bg-clip-text text-center mix-blend-difference invert"></h2>
+          <h2 className=" font-bold text-3xl"></h2>
         </div>
       </main>
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ function Header() {
 
   return (
     <header className="text-white w-full border-b border-gray-200">
-      <nav className="border-gray-200 bg-zinc-700">
+      <nav className="border-gray-200 bg-slate-700 ">
         <div className=" flex flex-wrap items-center justify-between mx-auto py-4 px-16">
           <a href="#" className="flex items-center">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white tracking-wider">
