c7d2279b13165f5068cffcabd9900e0d17d26bea
Kayro França <kayro.dev@gmail.com>
2023-08-06 20:02:10 -0300

    Add Projetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import Header from "@/components/Header";
+import Projetos from "@/components/Projetos";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-start w-full h-[85vh] relative md:p-40 bg-transparent">
-        <div className="grid gap-4 place-items-start p-8">
-          <h1 className="font-extrabold text-4xl md:text-5xl tracking-wide">
+      <main className="flex flex-col items-start w-full py-24 px-16 md:p-40 md:px-64">
+        <section className="grid gap-4 place-items-start h-[85vh] w-full">
+          <h1 className="font-extrabold text-2xl md:text-5xl tracking-wide">
             Olá, <br />
             Eu sou <span className="text-blue-900">Kayro França</span>. <br />
-            Um desenvolvedor web focado em frontend.
+            Um <span className="text-blue-900">desenvolvedor web</span> focado
+            em <span className="text-blue-900">frontend</span>.
           </h1>
-          <h2 className="font-bold text-3xl"></h2>
-        </div>
+          {/* <h2 className="font-bold text-3xl"></h2> */}
+        </section>
+        <Projetos />
       </main>
     </>
   );
