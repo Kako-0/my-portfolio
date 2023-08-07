d0aba88b158a9c95d14312e7ad561f75146a43fb
Kayro França <kayro.dev@gmail.com>
2023-08-07 17:43:36 -0300

    update styles, added new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
 import Projetos from "@/components/Projetos";
+import { ibmSerif } from "./fonts";
+import Sobre from "@/components/Sobre";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-start w-full py-24 px-16 md:p-40 md:px-64">
-        <section className="grid gap-4 place-items-start h-[85vh] w-full">
-          <h1 className="font-extrabold text-2xl md:text-5xl tracking-wide">
+      <main className="flex flex-col gap-12 items-start w-full py-24 px-16 md:p-8 md:px-64">
+        <section className="grid gap-4 place-items-center h-[90vh] w-full">
+          <h1
+            className={`font-bold italic text-2xl md:text-5xl tracking-wide ${ibmSerif.className}`}
+          >
             Olá, <br />
             Eu sou <span className="text-blue-900">Kayro França</span>. <br />
             Um <span className="text-blue-900">desenvolvedor web</span> focado
@@ -15,6 +19,7 @@ export default function Home() {
           </h1>
           {/* <h2 className="font-bold text-3xl"></h2> */}
         </section>
+        <Sobre />
         <Projetos />
       </main>
     </>
