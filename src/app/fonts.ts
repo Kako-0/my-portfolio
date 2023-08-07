11e4cb43789fdab57b02abf55a6a84a2e1a664ba
Kayro França <kayro.dev@gmail.com>
2023-08-07 17:40:49 -0300

    new font ibm serif

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter } from "next/font/google";
+import { IBM_Plex_Serif, Inter } from "next/font/google";
 import { Rozha_One } from "next/font/google";
 
 export const rozha = Rozha_One({ subsets: ["latin"], weight: "400", variable: '--font-rozha', });
 export const inter = Inter({ subsets: ["latin"] });
+export const ibmSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ["400" ,  "300" , "500" , "600" , "700"], style:["normal", "italic"]});
