10959f8203f9446c59a3da8ec5af9d12c84dac17
Kayro França <kayro.dev@gmail.com>
2023-08-08 16:52:26 -0300

    added styles, text

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -1,28 +1,33 @@
 export default function Sobre() {
   return (
     <section
-      className="grid gap-24 place-items-start w-full h-[80vh]"
+      className="grid gap-24 place-items-start w-full h-[90vh] bg-gradient-to-tr from-zinc-700 to-slate-800 clip-polygon text-neutral-200 pad-default"
       id="sobre"
     >
       <h3 className="font-bold text-3xl">Sobre mim</h3>
-      <div>
+      <div className="max-w-lg">
         <p>
-          I&apos;m a Frontend Web Developer building the Front-end of Websites
-          and Web Applications that leads to the success of the overall product.
-          Check out some of my work in the Projects section.
+          Com uma base em constante crescimento no campo da tecnologia, estou
+          empolgado em compartilhar minha jornada até o momento. Tenho me
+          concentrado em aprimorar minhas habilidades no desenvolvimento
+          front-end e aprofundar meu entendimento de UX. Criar experiências
+          significativas para os usuários é o que me motiva, impulsionando-me a
+          buscar constantemente novos desafios e oportunidades para expandir
+          meus conhecimentos e crescer profissionalmente.
         </p>
         <p>
-          I also like sharing content related to the stuff that I have learned
-          over the years in Web Development so it can help other people of the
-          Dev Community. Feel free to Connect or Follow me on my Linkedin where
-          I post useful content related to Web Development and Programming
+          Dentro do meu portfólio, você encontrará projetos que refletem meu
+          compromisso em desenvolver produtos digitais que superem as
+          expectativas dos usuários. Estou sempre à procura de soluções
+          criativas e das melhores práticas para enfrentar os desafios em
+          constante evolução no mundo digital. Se você procura alguém dedicado,
+          motivado e ansioso para contribuir com sua equipe, estou aberto a
+          explorar oportunidades que me permitam continuar aprendendo e
+          crescendo como profissional.
         </p>
-        <p>
-          I&apos;m open to Job opportunities where I can contribute, learn and
-          grow. If you have a good opportunity that matches my skills and
-          experience then don&apos;t hesitate to contact me.
-        </p>
+        <button className="btn">Resume</button>
       </div>
+      <div></div>
     </section>
   );
 }
