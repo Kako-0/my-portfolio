79d1a15e664e46d23aae35bf1f293fe0f13c2104
Kayro França <kayro.dev@gmail.com>
2023-08-09 19:47:14 -0300

    added h2 and Contato, Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,37 @@ import Header from "@/components/Header";
 import Projetos from "@/components/Projetos";
 import { ibmSerif } from "./fonts";
 import Sobre from "@/components/Sobre";
+import Footer from "@/components/Footer";
+import Contato from "@/components/Contato";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="flex flex-col gap-12 items-start w-full">
-        <section
-          className="grid gap-4 place-items-center h-[95vh] w-full pad-default"
-          id="inicio"
-        >
-          <h1
-            className={`font-bold italic text-2xl md:text-5xl tracking-wide ${ibmSerif.className}`}
-          >
-            Olá, <br />
-            Eu sou <span className="text-blue-900">Kayro França</span>. <br />
-            Um <span className="text-blue-900">desenvolvedor web</span> focado
-            em <span className="text-blue-900">frontend</span>.
-          </h1>
-          {/* <h2 className="font-bold text-3xl"></h2> */}
-        </section>
-        <Sobre />
-        <Projetos />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main className="flex flex-col gap-12 items-start w-full">
+				<section
+					className="flex flex-col items-start justify-center gap-8 h-[95vh] w-full pad-default"
+					id="inicio"
+				>
+					<h1
+						className={`font-bold italic text-3xl md:text-5xl tracking-wide ${ibmSerif.className}`}
+					>
+						Olá, <br />
+						Eu sou <span className="text-blue-900">Kayro França</span>. <br />
+						Um <span className="text-blue-900">desenvolvedor web</span> focado
+						em <span className="text-blue-900">frontend</span>.
+					</h1>
+					<h2 className="font-bold text-neutral-600 text-lg text-justify md:text-3xl md:max-w-5xl">
+						Com a paixão pelo UX e front-end, estou sempre explorando soluções
+						criativas e as melhores práticas para enfrentar desafios no mundo
+						digital.
+					</h2>
+				</section>
+				<Sobre />
+				<Projetos />
+				<Contato />
+			</main>
+			<Footer />
+		</>
+	);
 }
