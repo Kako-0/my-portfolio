421167e278800dd79c6932521b364aa090b63eba
Kayro França <kayro.dev@gmail.com>
2023-08-09 19:45:12 -0300

    updated style button

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -1,33 +1,33 @@
 export default function Sobre() {
-  return (
-    <section
-      className="grid gap-24 place-items-start w-full h-[90vh] bg-gradient-to-tr from-zinc-700 to-slate-800 clip-polygon text-neutral-200 pad-default"
-      id="sobre"
-    >
-      <h3 className="font-bold text-3xl">Sobre mim</h3>
-      <div className="max-w-lg">
-        <p>
-          Com uma base em constante crescimento no campo da tecnologia, estou
-          empolgado em compartilhar minha jornada até o momento. Tenho me
-          concentrado em aprimorar minhas habilidades no desenvolvimento
-          front-end e aprofundar meu entendimento de UX. Criar experiências
-          significativas para os usuários é o que me motiva, impulsionando-me a
-          buscar constantemente novos desafios e oportunidades para expandir
-          meus conhecimentos e crescer profissionalmente.
-        </p>
-        <p>
-          Dentro do meu portfólio, você encontrará projetos que refletem meu
-          compromisso em desenvolver produtos digitais que superem as
-          expectativas dos usuários. Estou sempre à procura de soluções
-          criativas e das melhores práticas para enfrentar os desafios em
-          constante evolução no mundo digital. Se você procura alguém dedicado,
-          motivado e ansioso para contribuir com sua equipe, estou aberto a
-          explorar oportunidades que me permitam continuar aprendendo e
-          crescendo como profissional.
-        </p>
-        <button className="btn">Resume</button>
-      </div>
-      <div></div>
-    </section>
-  );
+	return (
+		<section
+			className="grid gap-24 place-items-start w-full h-[90vh] bg-gradient-to-tr from-zinc-700 to-slate-800 clip-polygon text-neutral-200 pad-default"
+			id="sobre"
+		>
+			<h3 className="font-bold text-3xl">Sobre mim</h3>
+			<div className="max-w-lg">
+				<p>
+					Com uma base em constante crescimento no campo da tecnologia, estou
+					empolgado em compartilhar minha jornada até o momento. Tenho me
+					concentrado em aprimorar minhas habilidades no desenvolvimento
+					front-end e aprofundar meu entendimento de UX. Criar experiências
+					significativas para os usuários é o que me motiva, impulsionando-me a
+					buscar constantemente novos desafios e oportunidades para expandir
+					meus conhecimentos e crescer profissionalmente.
+				</p>
+				<p>
+					Dentro do meu portfólio, você encontrará projetos que refletem meu
+					compromisso em desenvolver produtos digitais que superem as
+					expectativas dos usuários. Estou sempre à procura de soluções
+					criativas e das melhores práticas para enfrentar os desafios em
+					constante evolução no mundo digital. Se você procura alguém dedicado,
+					motivado e ansioso para contribuir com sua equipe, estou aberto a
+					explorar oportunidades que me permitam continuar aprendendo e
+					crescendo como profissional.
+				</p>
+				<button className="btn-tonal">Resume</button>
+			</div>
+			<div></div>
+		</section>
+	);
 }
