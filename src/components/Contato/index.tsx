b73a02e0dbf4fc86ee0dda1ebcadeb9cd11a091b
Kayro França <kayro.dev@gmail.com>
2023-08-09 19:42:41 -0300

    new component Contato

---
+++ src/components/Contato/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Contato() {
+	return (
+		<section
+			className="grid gap-24 place-items-start w-full pad-default"
+			id="contato"
+		>
+			<h3 className="font-bold text-3xl">Contato</h3>
+		</section>
+	);
+}
