1437190e2ce3d2344608ba3b460f4121d7ba6896
Kayro França <kayro.dev@gmail.com>
2023-08-14 11:18:15 -0300

    new icons social medias

---
+++ src/assets/icons/EmailIcon.tsx
@@ -0,0 +1,45 @@
+export default function EmailIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			version="1.1"
+			width="256"
+			height="256"
+			viewBox="0 0 256 256"
+			xmlSpace="preserve"
+			shapeRendering="geometricPrecision"
+			textRendering="geometricPrecision"
+			imageRendering="optimizeQuality"
+			fillRule="evenodd"
+			className="w-[24px] h-[24px]"
+			clipRule="evenodd"
+			fill="currentColor"
+		>
+			<g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+				<path
+					d="M 46.003 52.103 c -0.285 0.257 -0.644 0.385 -1.003 0.385 s -0.718 -0.128 -1.003 -0.385 l -5.155 -4.637 L 23.741 60.208 h 42.517 l -15.1 -12.743 L 46.003 52.103 z"
+					fill="currentColor"
+					strokeLinecap="round"
+				/>
+				<polygon
+					points="66.32,29.79 23.68,29.79 45,48.97"
+					fill="currentColor"
+				/>
+				<polygon
+					points="21.07,31.48 21.07,58.53 36.59,45.44"
+					fill="currentColor"
+				/>
+				<polygon
+					points="68.93,58.53 68.93,31.48 53.41,45.44"
+					fill="currentColor"
+				/>
+				<path
+					d="M 45 0 C 20.147 0 0 20.147 0 45 c 0 24.853 20.147 45 45 45 s 45 -20.147 45 -45 C 90 20.147 69.853 0 45 0 z M 71.927 58.911 c 0 2.369 -1.928 4.297 -4.297 4.297 H 22.37 c -2.369 0 -4.296 -1.928 -4.296 -4.297 V 31.088 c 0 -2.369 1.927 -4.296 4.296 -4.296 h 45.26 c 2.369 0 4.297 1.927 4.297 4.296 V 58.911 z"
+					fill="currentColor"
+					strokeLinecap="round"
+				/>
+			</g>
+		</svg>
+	);
+}
